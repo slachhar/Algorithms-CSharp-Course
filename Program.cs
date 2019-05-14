b0fc03bab88340e66caa77058249b5f5b85cf5ef
Shefali Lachhar <shefali.lachhar@gmail.com>
2019-05-14 07:35:00 +0530

    Bubble Sort, Selection Sort, Insertion Sort and Nunit tests

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_DataStructure_Lib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(RecursiveFactorial(5));
+            Console.Read();
+        }
+
+        private static unsafe void IterateOver(int[] v)
+        {
+            fixed (int* b = v)
+            {
+                int* p = b;
+                for (int i = 0; i < v.Length; i++)
+                {
+                    Console.WriteLine(*p);
+                    p++;
+                }
+            }
+        }
+
+        static void ArrayTimeComplexity(object[] array)
+        {
+            //access by index O(1)
+            Console.WriteLine(array[0]);
+
+            int length = array.Length;
+            object elementINeedToFind = new object();
+            for (int i = 0; i < length; i++)
+            {
+                if (array[i] == elementINeedToFind)
+                {
+                    Console.WriteLine("Exists/Found");
+                }
+            }
+
+            //add to a full array
+            var bigArray = new int[length * 2];
+            Array.Copy(array, bigArray, length);
+            bigArray[length + 1] = 10;
+
+            //add to the end when there's some space
+            //O(1)
+            array[length - 1] = 10;
+
+            //O(1)
+            array[6] = null;
+        }
+
+        private static void RemoveAt(object[] array, int index)
+        {
+            var newArray = new object[array.Length - 1];
+        }
+
+        static string timeConversion(string s)
+        {
+            /*
+             * Write your code here.
+             */
+            string[] date = s.Split(':');
+            //12 AM : 00 constant 
+            //12 PM : 12 same
+            //01 AM : 01 same
+            //01 PM : 13 addition
+            //AM -> 12 : 00 , 01 : 01
+            //PM -> 12 : 12 , 01 : 13
+
+            string result = date[2].Substring(2, 2) == "AM" ? date[0] == "12" ? "00" : date[0] : date[0] == "12" ? "12" : (Convert.ToInt32(date[0]) + 12).ToString();
+
+
+            return result;
+
+        }
+
+        private static long RecursiveFactorial(int n)
+        {
+            long factorial = n;
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+                return factorial *= RecursiveFactorial(n-1);
+        }
+
+    }
+}
---
+++ Sorting.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_DataStructure_Lib
+{
+    public class Sorting
+    {
+        public static void BubbleSort(int[] array)
+        {
+            for (int partindex = array.Length - 1; partindex > 0; partindex--)
+            {
+                for (int i = 0; i < partindex; i++)
+                {
+                    if (array[i] > array[i + 1])
+                        Swap(array, i, i + 1);
+                }
+            }
+        }
+
+        public static void SelectionSort(int[] array)
+        {
+            for (int partindex = array.Length - 1; partindex > 0; partindex--)
+            {
+                int largest = 0;
+                for (int i = 1; i < partindex; i++)
+                {
+                    if (array[largest] <= array[i + 1])
+                    {
+                        largest = i + 1;
+                    }
+                }
+                Swap(array, largest, partindex);
+            }
+        }
+
+        public static void InsertionSort(int[] array)
+        {
+            for (int partindex = 1; partindex < array.Length; partindex++)
+            {
+                int curUnsorted = array[partindex];
+                int i = 0;
+                for (i = partindex; i > 0 && curUnsorted < array[i - 1]; i--)
+                {
+                    array[i] = array[i - 1];
+                }
+                array[i] = curUnsorted;
+            }
+        }
+
+        public static void ShellSort(int[] array)
+        {
+            int gap = array.Length / 2 + 1;
+            for (; gap > 0; gap = gap / 2)
+            {
+                for (int i = 0; i < array.Length && array[i] > array[i + gap]; i = i + gap)
+                {
+                    Swap(array, i, i + gap);
+                }
+            }
+        }
+
+        private static void Swap(int[] array, int i, int j)
+        {
+            if (i == j)
+                return;
+
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
