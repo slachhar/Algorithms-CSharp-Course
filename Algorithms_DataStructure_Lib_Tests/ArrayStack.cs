89b332c696964135b940acf02b2aaa921bed0f73
slachhar <shefali.lachhar@gmail.com>
2019-07-29 18:00:33 +0530

    array stack

---
+++ Algorithms_DataStructure_Lib_Tests/ArrayStack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Algorithms_DataStructure_Lib_Tests
+{
+	internal class ArrayStack<T>
+	{
+		public bool? IsEmpty { get; internal set; }
+
+		public int Count { get; set; }
+
+		public ArrayStack()
+		{
+		}
+
+		internal void Push(int v)
+		{
+			throw new NotImplementedException();
+		}
+
+		internal void Pop()
+		{
+			throw new NotImplementedException();
+		}
+
+		internal double Peek()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Algorithms_DataStructure_Lib_Tests/StackTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms_DataStructure_Lib_Tests
+{
+	[TestFixture]
+	public class StackTests
+	{
+		[Test]
+		public void IsEmpty_EmptyStack_ReturnsTrue()
+		{
+			var stack = new ArrayStack<int>();
+			Assert.IsTrue(stack.IsEmpty);
+		}
+
+		public void Count_PushOneItem_ReturnsOne()
+		{
+			var stack = new ArrayStack<int>();
+			stack.Push(1);
+			Assert.AreEqual(1, stack.Count);
+			Assert.IsFalse(stack.IsEmpty);
+		}
+
+		public void Peek_PushTwoItemsAndPop_ReturnsHeadElement()
+		{
+			var stack = new ArrayStack<int>();
+			stack.Push(1);
+			stack.Push(2);
+			stack.Pop();
+			Assert.AreEqual(1, stack.Peek());
+		}
+	}
+}
