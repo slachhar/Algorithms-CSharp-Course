434b4769bcff9a660b735bcbcae630bbad3ecdbb
slachhar <shefali.lachhar@gmail.com>
2019-09-24 18:10:50 +0530

    array queue

--- ArrayQueue.cs
+++ ArrayQueue.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,17 @@ using System.Threading.Tasks;
 
 namespace Algorithms_CSharp_Course
 {
-	public class ArrayQueue<T>
+	public class ArrayQueue<T> : IEnumerable<T>
 	{
+		private T[] _queue;
+		public IEnumerator<T> GetEnumerator()
+		{
+			throw new NotImplementedException();
+		}
 
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
 	}
 }
